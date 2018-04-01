1c1532a61f757dc6e06704aac90a4033703767a8
Edoardo <edoardo.fullin@hotmail.it>
2018-04-01 18:21:16 +0200

    CommunicationManger send data

--- Server/Networking/CommunicationsManager.cs
+++ Server/Networking/CommunicationsManager.cs
@@ -14,21 +14,26 @@ namespace QuizMaster___Server.Networking {
 
 	    public const int SERVER_PORT = 19999;
 
-
 	    private readonly TcpListener listener;
+	    private readonly TcpClient tcpClient;
+
 	    private readonly Thread listenThread;
 
-		/// <param name="client">The client from which the connection is received</param>
+		/// <param name="client">The tcpClient from which the connection is received</param>
 		/// <param name="message">The message (usually a message)</param>
 	    public delegate void MessageReceivedEventArgs(Client client, string message);
 
 		/// <summary>
-		/// Raised when a message is received from a client
+		/// Raised when a message is received from a tcpClient
 		/// </summary>
 	    public event MessageReceivedEventArgs MessageReceived;
 
+		/// <summary>
+		/// Main Constructor
+		/// </summary>
 	    public CommunicationsManager() {
 		    listener = new TcpListener(IPAddress.Any, SERVER_PORT);
+			tcpClient = new TcpClient();
 		    listenThread = new Thread(ListenForData);
 		}
 
@@ -39,26 +44,53 @@ namespace QuizMaster___Server.Networking {
 		    listenThread.Start();
 		}
 
+		/// <summary>
+		/// Stop the server
+		/// </summary>
 	    public void Stop() {
 			listenThread.Abort();
 	    }
 
+		/// <summary>
+		/// Sends data to client and does not wait for a response
+		/// </summary>
+		/// <param name="client">Client to send data to</param>
+		/// <param name="data">the JSON-formatted data you want to send</param>
+	    public async Task SendData(Client client, string data) {
+			await tcpClient.ConnectAsync(client.IPAddress, SERVER_PORT);
+		    var stream = tcpClient.GetStream();
+		    var bytes = Encoding.UTF8.GetBytes(data);
+			stream.Write(bytes,0, bytes.Length);
+	    }
+
+		/// <summary>
+		/// Sends data to client and does wait for a response
+		/// </summary>
+		///<param name="client">Client to send data to</param>
+		/// <param name="data">the JSON-formatted data you want to send</param>
+		/// <returns>The JSON-formatted response</returns>
+		public async Task<string> GetResponse(Client client, string data) {
+		    await SendData(client, data);
+		    return ReceiveJson(tcpClient.Client);
+	    }
+
+
 	    private void ListenForData() {
-			listener.Start();
+		    listener.Start();
 
 		    while (listenThread.ThreadState != ThreadState.AbortRequested) {
 			    Socket client = listener.AcceptSocket();
-				var task = new Task(() => {
-					MessageReceived?.Invoke(null, ReceiveJson(client));			
-				});
-				task.Start();
+			    var task = new Task(() => {
+				    MessageReceived?.Invoke(null, ReceiveJson(client));
+			    });
+			    task.Start();
 		    }
 
-			listener.Stop();
+		    listener.Stop();
 	    }
 
 	    private string ReceiveJson(Socket socket) {
-			var buffer = new List<byte>();
+		    var buffer = new List<byte>();
 
 		    while (socket.Available > 0) {
 			    var currByte = new Byte[1];
@@ -71,5 +103,6 @@ namespace QuizMaster___Server.Networking {
 
 		    return Encoding.UTF8.GetString(buffer.ToArray());
 	    }
-    }
+
+	}
 }
