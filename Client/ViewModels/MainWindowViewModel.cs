72d1c3563f429c3647eb56e0c2fd750869c2f4b3
Edoardo <edoardo.fullin@hotmail.it>
2018-04-23 20:59:34 +0200

    UDP Broadcast (does not work) oidocrop

--- Client/ViewModels/MainWindowViewModel.cs
+++ Client/ViewModels/MainWindowViewModel.cs
@@ -1,23 +1,61 @@
 ﻿using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using QuizMaster___Client.Models;
 using QuizMaster___SharedLibrary.MVVM;
+using QuizMaster___SharedLibrary.Networking;
+using QuizMaster___SharedLibrary.Packets;
 
 namespace QuizMaster___Client.ViewModels {
 	public class MainWindowViewModel {
-
 		public Client MeClient { get; set; }
+
+		public string StateText { get; private set; }
+
+		private string serverIP;
+
 		public RelayCommand AvviaTestCommand { get; private set; }
 
 		public MainWindowViewModel() {
 			MeClient = new Client();
-			AvviaTestCommand = new RelayCommand(AvviaTest, o =>
-					true
-				//BUG !(string.IsNullOrEmpty(MeClient.Cognome) || string.IsNullOrEmpty(MeClient.Nome))
-			);
+			this.StateText = "Cercando il server...";
+			AvviaTestCommand = new RelayCommand(AvviaTest, o => true);
+			SearchServer();
 		}
 
 		private void AvviaTest(object obj) {
 			Debug.WriteLine("Avviato test");
+		}
+
+		private async Task SearchServer() {
+
+			foreach (var _interface in NetworkInterface.GetAllNetworkInterfaces()) {
+
+				foreach (var address in _interface.GetIPProperties().UnicastAddresses) {
+
+					var broadcast = Networking.GetBroadcastAddress(address);
+					var client = new UdpClient();
+
+					var bytes = Encoding.UTF8.GetBytes(new RequestPacket("getserverinfo").Serialize());
+
+					await client.SendAsync(bytes, bytes.Length, new IPEndPoint(broadcast, 19999));
+
+					client.ReceiveAsync().ContinueWith((task => {
+						var packet = JsonConvert.DeserializeObject<DictionaryPacket>(Encoding.UTF8.GetString(task.Result.Buffer));
+						Debug.WriteLine(packet.Serialize());
+						serverIP = packet["serverIP"] as string;
+					}));
+
+
+				}
+
+			}
+
+
 		}
 	}
 }
---
+++ Commons/SharedLibrary/Networking/Networking.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace QuizMaster___SharedLibrary.Networking
+{
+    public static class Networking {
+
+	    public static async Task<string> GetLocalIPAddressAsync() {
+		    using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0)) {
+				socket.Connect("8.8.8.8", 65530);
+			    var endPoint = socket.LocalEndPoint as IPEndPoint;
+			    return endPoint?.Address.ToString();
+		    }
+	    }
+
+	    public static IPAddress GetBroadcastAddress(UnicastIPAddressInformation unicastAddress) {
+		    return GetBroadcastAddress(unicastAddress.Address, unicastAddress.IPv4Mask);
+	    }
+
+	    public static IPAddress GetBroadcastAddress(IPAddress address, IPAddress mask) {
+		    uint ipAddress = BitConverter.ToUInt32(address.GetAddressBytes(), 0);
+		    uint ipMaskV4 = BitConverter.ToUInt32(mask.GetAddressBytes(), 0);
+		    uint broadCastIpAddress = ipAddress | ~ipMaskV4;
+
+		    return new IPAddress(BitConverter.GetBytes(broadCastIpAddress));
+	    }
+
+	}
+}
